d2bd841f154904af34b07101b0bcd20d375b0ffb
Matthew Gazzera <genzu44@gmail.com>
2021-03-16 13:28:49 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// SAME KEYS AND VALUES
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+// ES2015 VERSION
+
+function createInstructor() {}
+
+// COMPUTED PROPERTY NAMES
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+// ES2105 VERSION
+
+function favoriteNumber() {}
+
+// Object Methods
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+
+// ES2015 VERSION
+
+function instructorSays() {}
+
+// createAnimal function
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+// const d = createAnimal("dog", "bark", "Woooof!")
+// // {species: "dog", bark: ƒ}
+// d.bark()  //"Woooof!"
+
+// const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// // {species: "sheep", bleet: ƒ}
+// s.bleet() //"BAAAAaaaa"
